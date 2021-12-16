2edbe1b718e29fdad30dc6309676b8c344cfd318
Jean Wong <jean5572@stud.kea.dk>
2021-12-16 15:39:42 +0100

    booking done

---
+++ js/booking.js
@@ -0,0 +1,70 @@
+//FILTER
+let filter = "alle";
+let produkter;
+let container = document.querySelector("#booking_container");
+let temp = document.querySelector("template");
+
+//URL + ID
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const link = "http://felixbryld.dk/kea/final_exam//wp-json/wp/v2/prisliste?per_page=100";
+
+//Load Site + function
+document.addEventListener("DOMContentLoaded", hentData);
+
+async function hentData() {
+  const respons = await fetch(link);
+  produkter = await respons.json();
+  addEventListenersToButtons();
+  vis(produkter);
+}
+
+function vis(produkter) {
+    //Løber igennem array "produkter" fra JSON (WP)
+    container.innerHTML = "";
+    //forEach Loop
+    produkter.forEach((produkt) => {
+        if (filter == "alle" || filter == produkt.categories) {
+            console.log(produkt);
+            const klon = temp.cloneNode(true).content;
+            klon.querySelector(".behandling_navn").textContent = produkt.behandlingsnavn;
+            klon.querySelector(".behandling_pris").textContent = produkt.pris + " kr.";
+            if (produkt.billede !== false) {
+                klon.querySelector(".behandling_billede").src = produkt.billede.guid;
+            }
+          klon.querySelector("article").addEventListener("click", () => visDetaljer(produkt));
+
+          container.appendChild(klon);
+        }
+    });
+    //Alle produkters ID
+    produkter.forEach((produkt) => {
+        if (id == produkt.id) {
+            visDetaljer(produkt);
+         }
+    });
+}
+
+//Location.href til Singlview
+function visDetaljer(produkt) {
+  location.href = `singleview.html?id=${produkt.id}`;
+}
+
+//Kommer fra hentData
+function addEventListenersToButtons() {
+  document.querySelectorAll(".filter").forEach((btn) => {
+    btn.addEventListener("click", filterBTNs);
+  });
+}
+
+//tilføj kategori til knap
+function filterBTNs() {
+  filter = this.dataset.item;
+  document.querySelectorAll(".filter").forEach((btn) => {
+    btn.classList.remove("valgt");
+  });
+
+  this.classList.add("valgt");
+  vis(produkter);
+}
